c6fe1519896468f5dde8cf8ecbc7674fa81fbadb
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-09-12 15:08:05 +0530

    (feat): add header

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -7,14 +7,21 @@ defineProps<{
 </script>
 
 <template>
-  <div class="container-bordered">{{ $props.title }}</div>
+  <div class="container-bordered">
+    <div class="header">
+      <h1 class="title">
+        {{ $props.title }}
+      </h1>
+      <button>Add Tasks</button>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .container-bordered {
   display: flex;
   flex-direction: column;
-  padding: 10px;
+  padding: 10px 15px;
   max-width: 400px;
   width: 90%;
 
@@ -22,5 +29,28 @@ defineProps<{
   border-radius: 5px;
 
   text-align: left;
+
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    .title {
+      font-size: 1.25rem;
+      line-height: 0.5rem;
+    }
+    button {
+      border: 1px solid #ddd;
+      padding: 5px 10px;
+      border-radius: 5px;
+      background-color: transparent;
+      text-transform: uppercase;
+      transition-duration: 300ms;
+      &:hover {
+        cursor: pointer;
+        box-shadow: 0 2px 5px #86e9bc77;
+      }
+    }
+  }
 }
 </style>
