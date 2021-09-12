37b67fd4c5025090a018d1c9a91dd5b7db055218
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-09-12 14:06:32 +0530

    (feat): init vue

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="home">
+    <h1>About this project</h1>
+    <p>A simple app to manage your tasks.</p>
+    <p>Made with 🍖 by <span class="name">Abhinav Robinson.</span></p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  .name {
+    color: #42b983;
+    font-weight: bold;
+  }
+}
+</style>
